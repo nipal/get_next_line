249d922ff1be2059e5b5ac64e3426f16c45b0cda
Fabrice JANOTY <fjanoty@e1r4p21.42.fr>
2016-03-02 06:21:16 +0100

    debut de transformation du copie_line sans le free

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/17 21:53:11 by fjanoty           #+#    #+#             */
-/*   Updated: 2016/03/02 04:45:39 by fjanoty          ###   ########.fr       */
+/*   Updated: 2016/03/02 06:20:21 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,32 @@
 /*
  *	si on copy le dernier element, on peut utiliser lstdel
  * */
-static	char	*copy_line_and_free_lst(t_list *elem, char **dest, int nb_last)
+static	char	*copy_line(t_list *begin, char **dest, int nb_last)
 {
 	int		rest;
 	int		i;
 	t_list	*tmp;
+	t_list	*elem;
 	char	*stmp;
 
 	rest = 0;
-	while (elem->next)
+	elem = begin;
+	if (elem)
 	{
-		tmp = elem->next;
-		ft_memcopy(*dest + rest, (char*)elem->content, BUFF_SIZE);
-		rest += BUFF_SIZE;
+		while (elem->next)
+		{
+			tmp = elem->next;
+			ft_memcopy(*dest + rest, (char*)elem->content, BUFF_SIZE);
+			rest += BUFF_SIZE;
+			free(elem->content);
+			free(elem);
+			elem = tmp;
+		}
+		i = ((strchr((char*)elem->content), TARGET_CHAR) - (char*)elem->content);
+		ft_memcopy(*dest + rest, elem->content, (size_t)i);
+		stmp = (ft_strlen((char*)(elem->content + i))) ? ft_strdup((char*)((elem->content + i))) : NULL;
 		free(elem->content);
-		free(elem);
-		elem = tmp;
 	}
-	i = ((strchr((char*)elem->content), TARGET_CHAR) - (char*)elem->content);
-	ft_memcopy(*dest + rest, elem->content, (size_t)i);
-	stmp = (ft_strlen((char*)(elem->content + i))) ? ft_strdup((char*)((elem->content + i))) : NULL;
-	free(elem->content);
 	return = (stmp);
 }
 
@@ -55,8 +60,6 @@ static	t_list	*fill_line(t_list *begin, int fd)
 	while (elem)
 	{
 		elem->content_size += read(fd, elem->content, BUFF_SIZE - elem->content_size);
-		//	on a pas touver de \n mais il reste de truc a lire dans le fichier
-		//	donc il faut un nouveau chainonx avec unnouveau buffer et un curseur (buff_size)
 		if ((i = (ft_strchr((char*)elem->content, TARGET_CHAR) - (char*)elem->content)) < 0
 				&& elem->content_size == BUFF_SIZE)
 		{
@@ -64,23 +67,8 @@ static	t_list	*fill_line(t_list *begin, int fd)
 				return (NULL);
 			str[BUFF_SIZE] = '\0';
 			elem->next = ft_lstnew(str, 0);
-			elem = elem->next;
 		}
-
-		// 	Si	(on est a la fin du fichier) ET (on a pas trouver de \n)
-		else if (i < 0)
-		{
-			//	la le travaille est terminer ET on connai la taille du dernier bufer
-			//	qui est enregistre dans le content_size
-			//	donc juste un return begin
-			return (begin);
-		}
-
-		//	on a trouver un \n. Donc on a plus besoin d'accumuller de nouvaux buffer.
-		else
-		{
-			return (begin);
-		}
+		elem = elem->next;
 	}
 	return (begin);
 }
